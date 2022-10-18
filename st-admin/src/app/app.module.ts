30538431b37be685f8765dad4bde759c9838cbb0
Reja Kazi <rejakazi02@gmail.com>
2022-10-18 11:19:31 +0600

    add

--- st-admin/src/app/app.module.ts
+++ st-admin/src/app/app.module.ts
@@ -55,7 +55,7 @@ import { IconModule, IconSetService } from '@coreui/icons-angular';
 import { ProfileComponent } from './containers/default-layout/profile/profile.component';
 import { FormsModule } from '@angular/forms';
 import { AdminComponent } from './admin/admin.component';
-import { NgSelect2Module } from 'ng-select2';
+
 
 
 
@@ -102,7 +102,7 @@ const APP_CONTAINERS = [
     MatIconModule,
     HttpClientModule,
     FormsModule,
-    NgSelect2Module,
+ 
 
   ],
   providers: [
