4cafb5ea2aa17852e90c2ee6ed7d446720c3bd8d
Reja Kazi <rejakazi02@gmail.com>
2022-10-30 16:09:00 +0600

    add teacher issues

--- st-admin/src/app/views/teacher/teacher-add/teacher-add.component.ts
+++ st-admin/src/app/views/teacher/teacher-add/teacher-add.component.ts
@@ -1,7 +1,6 @@
 import { TeacherService } from './../../../service/teacher.service';
 import { Component, OnInit } from '@angular/core';
 
-
 import {
   FormControl,
   FormBuilder,
@@ -14,25 +13,25 @@ import { param } from 'jquery';
 import { ToastrService } from 'ngx-toastr';
 import { MatSelectChange } from '@angular/material/select';
 
-
 @Component({
   selector: 'app-teacher-add',
   templateUrl: './teacher-add.component.html',
-  styleUrls: ['./teacher-add.component.scss']
+  styleUrls: ['./teacher-add.component.scss'],
 })
 export class TeacherAddComponent implements OnInit {
-  teacherAdd!:FormGroup;
+  teacherAdd!: FormGroup;
   responceData: any;
-  undata:any;
+  undata: any;
   // unionName:any;
-  errorMessage:any;
+  errorMessage: any;
   unionName?: any[];
   filteredUnionList?: any[];
-  
-  constructor(private fb: FormBuilder,
+
+  constructor(
+    private fb: FormBuilder,
     private teaService: TeacherService,
-    private toastr: ToastrService,
-    ) { }
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.teacherAdd = this.fb.group({
@@ -40,68 +39,57 @@ export class TeacherAddComponent implements OnInit {
       phone: ['', Validators.required],
       department_id: ['', Validators.required],
       joining_date: ['', Validators.required],
-     
+
       gender: ['', Validators.required],
       union_id: ['', Validators.required],
       present_address: ['', Validators.required],
       password: ['', Validators.required],
-     
     });
 
-this.unionData();
-
+    this.unionData();
   }
 
+  teaSubmit() {
+    console.log('test', this.teacherAdd.value);
+    this.teaService.teaPost(this.teacherAdd.value).subscribe(
+      (result) => {
+        this.responceData = result;
 
-  
-  teaSubmit(){
-    console.log("test", this.teacherAdd.value)
-    this.teaService.teaPost(this.teacherAdd.value).subscribe((result) => {
-      this.responceData = result;
-   
+        this.teacherAdd.reset();
+        this.toastr.success(result.message);
+        this.errorMessage = null;
+      },
+      (err) => {
+        this.errorMessage = err.error.errors;
 
-      
-      this.teacherAdd.reset();
-      this.toastr.success(result.message);
-          this.errorMessage=null;
-        },
-        (err)=>{
-          this.errorMessage=err.error.errors;
-
-          if(err.error.errors.name){
-            this.toastr.error(err.error.errors.name);
-          }
-          if(err.error.errors.phone){
-            this.toastr.error(err.error.errors.phone);
-          }
-          if(err.error.errors.department_id){
-            this.toastr.error(err.error.errors.department_id);
-          }
-          if(err.error.errors.joining_date){
-            this.toastr.error(err.error.errors.joining_date);
-          }
-          if(err.error.errors.gender){
-            this.toastr.error(err.error.errors.gender);
-          }
-          if(err.error.errors.present_address_union_id){
-            this.toastr.error(err.error.errors.present_address_union_id);
-          }
-          if(err.error.errors.present_address){
-            this.toastr.error(err.error.errors.present_address);
-          }
-          if(err.error.errors.password){
-            this.toastr.error(err.error.errors.password);
-          }
-
-      
-         
-        });
-    
+        if (err.error.errors.name) {
+          this.toastr.error(err.error.errors.name);
+        }
+        if (err.error.errors.phone) {
+          this.toastr.error(err.error.errors.phone);
+        }
+        if (err.error.errors.department_id) {
+          this.toastr.error(err.error.errors.department_id);
+        }
+        if (err.error.errors.joining_date) {
+          this.toastr.error(err.error.errors.joining_date);
+        }
+        if (err.error.errors.gender) {
+          this.toastr.error(err.error.errors.gender);
+        }
+        if (err.error.errors.present_address_union_id) {
+          this.toastr.error(err.error.errors.present_address_union_id);
+        }
+        if (err.error.errors.present_address) {
+          this.toastr.error(err.error.errors.present_address);
+        }
+        if (err.error.errors.password) {
+          this.toastr.error(err.error.errors.password);
+        }
+      }
+    );
   }
 
-
-
-  
   // union code chere
 
   unionData() {
@@ -109,8 +97,7 @@ this.unionData();
       next: (result) => {
         this.undata = result;
         this.unionName = this.undata.data;
-        console.log("union", this.unionName);
-        
+
         this.filteredUnionList = this.unionName?.slice();
       },
       error: (err) => {
