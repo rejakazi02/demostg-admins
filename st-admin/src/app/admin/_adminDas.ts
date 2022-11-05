816e33edb168f0c05695ab23d04d7aa78cc303fe
Reja Kazi <rejakazi02@gmail.com>
2022-11-05 18:04:12 +0600

    Add

--- st-admin/src/app/admin/_adminDas.ts
+++ st-admin/src/app/admin/_adminDas.ts
@@ -4,7 +4,7 @@ import { MatIcon } from '@angular/material/icon';
 export const navItems: INavData[] = [
   {
     name: 'Dashboard',
-    url: './dashboard',
+    url: './admin',
     iconComponent: { name: 'cil-speedometer' },
     badge: {
       color: 'info',
