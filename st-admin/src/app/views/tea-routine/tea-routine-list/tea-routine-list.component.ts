391d2c90803152a4c029c65450e02b44f28d185a
Reja Kazi <rejakazi02@gmail.com>
2022-11-19 13:09:39 +0600

    Add teacher class routine

--- st-admin/src/app/views/tea-routine/tea-routine-list/tea-routine-list.component.ts
+++ st-admin/src/app/views/tea-routine/tea-routine-list/tea-routine-list.component.ts
@@ -1,5 +1,6 @@
+import { ClassService } from 'src/app/service/class.service';
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-tea-routine-list',
   templateUrl: './tea-routine-list.component.html',
@@ -7,9 +8,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeaRoutineListComponent implements OnInit {
 
-  constructor() { }
+  classRoutinesData:any;
+  classDatas:any;
+id: any;
+classSectionData:any;
+classRoutinesSearchData:any
+  constructor(
+    private classService: ClassService
+  ) { }
 
   ngOnInit(): void {
+    this.classRoutinesList();
+    this.classData();
+    
   }
+
+  // class Routines List
+  classRoutinesList() {
+
+    // if(this.classRoutinesSearchData){
+    //   this.search
+    // }
+   
+      this.classService.classRoutinesList().subscribe((result) => {
+        this.classRoutinesData = result;
+        console.log('teaData', this.classRoutinesData);
+       
+      });
+    
+   
+  } 
+
+   // classs data 
+   classData(){
+    this.classService.classData().subscribe((result)=>{
+      
+      this.classDatas = result;
+
+    })
+  }
+
+
+  getSection(value?:any){
+
+    this.classService
+    .SubSectDat(value)
+    .subscribe((result) => {
+      this.classSectionData = result;
+      console.log('sec',  this.classSectionData);
+      
+    });
+
+  }
+
+  search(day:any, classsId:any, sectionId:any){
+    
+    this.classService.classRoutinesSearch(classsId, sectionId, day).subscribe((result) => {
+      this.classRoutinesData = result;
+      console.log('classRoutinesSearchData', this.classRoutinesData);
+     
+    });
+
+
+  }
+
+
+
+// delete section 
+  confirmBox(id: any) {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+         
+        );
+     
+
+        this.classService.deleteClassRoutineData(id).subscribe((result) => {
+          // window.location.reload();
+          console.log('dlt',result);
+          
+          this.classRoutinesList();
+        
+        });
+       
+        
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+      }
+    });
+  }
+
+
 
 }
