2c06dbd4e5b8989a89f9c334ec660db3a4e7e8b3
Reja Kazi <rejakazi02@gmail.com>
2022-12-05 13:49:07 +0600

    Add

--- st-admin/src/app/auth/login/login.component.ts
+++ st-admin/src/app/auth/login/login.component.ts
@@ -33,83 +33,61 @@ export class LoginComponent implements OnInit {
     // console.log(this.logInForm.value);
 
     if (this.logInForm.valid) {
-      this.authService.anyUserLogin(this.logInForm.value).subscribe(
-        (result) => {
-          if (result != null) {
-            this.responceData = result;
-            console.log('admin', this.responceData);
-            
+     
 
-            localStorage.setItem('token', this.responceData?.access_token);
-            localStorage.setItem('role', this.responceData?.role);
 
-            // console.log('login token',this.responceData.access_token);
-            if (this.responceData?.role === 'admin') {
-              this.route.navigate(['/', 'admin']);
+        this.authService.proceedLogin(this.logInForm.value).subscribe(
+          (result) => {
+            if (result != null) {
+              this.responceData = result;
+              console.log('this');
+              console.log(' super admin', this.responceData);
+              localStorage.setItem('token', this.responceData.access_token);
+  
+              this.route.navigate(['/', 'superAdmin']);
+  
               this.toastr.success(result.message);
-              console.log('user Id:', this.responceData?.role);
             }
-            if (this.responceData?.role === 'student') {
-              this.route.navigate(['/', 'student']);
-              this.toastr.success(result.message);
-              console.log('user Id:', this.responceData?.role);
-            }
-            if (this.responceData?.role === 'staff') {
-              this.route.navigate(['/', 'teacher-dashboard']);
-              this.toastr.success(result.message);
-              console.log('user Id:', this.responceData?.role);
-            }
-          }
-        },
+          },
         (err) => {
-
-          console.log('admin error');
-          // if(this.responceData?.role === 'admin' || this.responceData?.role === 'staff' || this.responceData?.role === 'student'){
-
-            this.errorMessage = err.error.errors;
-            console.log('err.error', err);
-            
-            if (err.error.errors.email) {
-              this.toastr.error(err.error.errors.email);
-            }
-            if (err.error.errors.password) {
-              this.toastr.error(err.error.errors.password);
-            }
-
-          // }
-         
-
-          // alert(err.error.message)
-        }
-      );
-    } 
-     if (this.logInForm.valid) {
-      this.authService.proceedLogin(this.logInForm.value).subscribe(
-        (result) => {
-          if (result != null) {
-            this.responceData = result;
-            console.log('this');
-            console.log(' super admin', this.responceData);
-            localStorage.setItem('token', this.responceData.access_token);
-
-            this.route.navigate(['/', 'superAdmin']);
-
-            this.toastr.success(result.message);
+          this.authService.anyUserLogin(this.logInForm.value).subscribe(
+            (result) => {
+              if (result != null) {
+                this.responceData = result;
+                // console.log('admin', this.responceData);
+    
+                localStorage.setItem('token', this.responceData?.access_token);
+                localStorage.setItem('role', this.responceData?.role);
+    
+                // console.log('login token',this.responceData.access_token);
+                if (this.responceData?.role === 'admin') {
+                  this.route.navigate(['/', 'admin']);
+                  this.toastr.success(result.message);
+                  console.log('user Id:', this.responceData?.role);
+                }
+                if (this.responceData?.role === 'student') {
+                  this.route.navigate(['/', 'student']);
+                  this.toastr.success(result.message);
+                  console.log('user Id:', this.responceData?.role);
+                }
+                if (this.responceData?.role === 'staff') {
+                  this.route.navigate(['/', 'teacher-dashboard']);
+                  this.toastr.success(result.message);
+                  console.log('user Id:', this.responceData?.role);
+                }
+               
+              }
+            },
+            (err) => {
+              this.errorMessage=err.error.errors;
+          if(err.error.errors.email){
+            this.toastr.error(err.error.errors.email);
           }
-        },
-        (err) => {
-
-          if(this.responceData?.adminData?.name === 'Super Admin'){
-
-            this.errorMessage = err.error.errors;
-            if (err.error.errors.email) {
-              this.toastr.error(err.error.errors.email);
-            }
-            if (err.error.errors.password) {
-              this.toastr.error(err.error.errors.password);
-            }
+          if(err.error.errors.password){
+            this.toastr.error(err.error.errors.password);
           }
-         
+            }
+          );
         }
       );
     }
